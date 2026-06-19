1c8fa11e5397fbefa5f7212bddfc3299cceeb595
261166 <289014476+261166@users.noreply.github.com>
2026-06-19 21:48:01 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 
 
 def add(n1, n2):
-    sum = n1 + n2
-
+    return n1 + n2
 
 print("Welcome to the calculator\n")
-input("What operation would you like to perform?\nSum - type '1'\n")
+calc_on = 1
 
-
+while calc_on != 0:
+    choice = int(input("What operation would you like to perform?\nSum - type '1'\nExit - 0\n"))
+    if choice == 1:
+        num1 = int(input("Type in the first number:\n"))
+        num2 = int(input("Type in the second number:\n"))
+        answer = int(add(num1, num2))
+        print("The answer:\n", num1, "+", num2, "=", answer, "\n")
+    elif choice == 0:
+        print("Goodbye")
+        calc_on = 0
